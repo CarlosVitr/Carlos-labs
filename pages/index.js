ebdc0637434384c5043a315db937d2474909ca78
Carlos Vitor <43506171+CarlosVitr@users.noreply.github.com>
2020-09-06 20:38:40 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://carlosvitor.ml"> Carlos Labs </a>
+          Welcome to <a href="http://carlosvitor.ml/"> Carlos Labs </a>
         </h1>
 
         <p className={styles.description}>
